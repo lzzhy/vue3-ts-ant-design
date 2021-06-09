bc84f7d3540bd9092383ff8e3a794e130ebb7222
liting <liting@example.com>
2021-06-09 16:07:12 +0800

    feat: 删除多余页面

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,15 @@
 <template>
 <div>
   <a-button type="primary">Primary</a-button>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
   }
 })
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Home from '@/views/home.vue'
+// import Home from '@/views/home.vue'
 import Vuex from '@/views/vuex.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
   {
     path: '/vuex',
     name: 'Vuex',
